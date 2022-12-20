255985099b6ea8d2e7055072b171e33b16a3d4e2
daanengbers <daan.engbers@gmail.com>
2022-12-20 14:22:03 +0100

    Git init + unity vr init

---
+++ VRCourse/Assets/Scripts/HandControllerScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandControllerScript : MonoBehaviour
+{
+
+    [SerializeField] InputActionReference gripInputAction;
+    [SerializeField] InputActionReference triggerInputAction;
+
+}
